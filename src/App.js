0f8d8fbec92a7fbc7f3eaef08477c10551fa3ec5
Wayne Lee <waynel1996weiyang@gmail.com>
2020-11-20 10:59:00 -0800

    Set up basic navbar, include bootstrap to index.js, set up custom font to be used.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css"
 import { BrowserRouter as Router, Route } from "react-router-dom"
-
+import Nav from "./components/Nav"
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Nav />
         <Route exact path="/">
           <p>Main Page</p>
         </Route>
