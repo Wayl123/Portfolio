b1020223c42dd8ed0972ad55a37b372182ad2ab1
Wayne Lee <waynel1996weiyang@gmail.com>
2020-11-21 14:13:56 -0800

    Some basic styling, not sure how to change card styling so I will leave that be for now.

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -15,8 +15,8 @@ export default class Project extends Component {
   render() {
     return (
       <div className="content-container d-flex flex-column align-items-center overflow-auto">
-        <h2>Projects</h2>
         <div className="project-container d-flex justify-content-around align-items-center flex-wrap">
+          <h2 className="project-head align-self-start p-3">Projects</h2>
           {projectContent.map(displayProject)}
         </div>
       </div>
--- src/components/Project/ProjectItem.js
+++ src/components/Project/ProjectItem.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default class ProjectItem extends Component {
   render() {
     return (
-      <div className="project-card">
+      <div className="project-card p-3">
         <div className="card">
           <Link to="/Project" className="img-wrap">
             <img src={this.props.img} className="card-img-top image"/> 
